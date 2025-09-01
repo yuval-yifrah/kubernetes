36318c6ebd524b451a938cdda2af96a3ca6268c3
EC2 Default User <ec2-user@ip-10-0-1-123.ec2.internal>
2025-09-01 12:07:12 +0000

    run file that does port forward

--- mini_kube_deployment/run_all.sh
+++ mini_kube_deployment/run_all.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+#delete old pods
+kubectl delete namespace wordpress monitoring
+kubectl wait --for=delete namespace wordpress --timeout=180s
+kubectl wait --for=delete namespace monitoring --timeout=180s
+
 # --- Namespaces ---
 kubectl apply -f wordpress/namespace.yml
 kubectl apply -f monitoring/namespace.yml
@@ -27,4 +32,13 @@ kubectl apply -f monitoring/prometheus_service.yml
 kubectl apply -f monitoring/prometheus_ingress.yml
 
 echo "All resources applied successfully!"
+#waits for the pods to be ready
+kubectl wait --for=condition=ready pod -l app=wordpress -n wordpress --timeout=300s
+kubectl wait --for=condition=ready pod -l app=grafana -n monitoring --timeout=300s
+kubectl wait --for=condition=ready pod -l app=prometheus -n monitoring --timeout=300s
+
+#portforwarding them
+kubectl port-forward -n wordpress svc/wordpress-service 8080:80 --address=0.0.0.0 &
+kubectl port-forward -n monitoring svc/monitoring-grafana 3000:80 --address=0.0.0.0 &
+kubectl port-forward -n monitoring svc/prometheus-service 9090:9090 --address=0.0.0.0 &
 
